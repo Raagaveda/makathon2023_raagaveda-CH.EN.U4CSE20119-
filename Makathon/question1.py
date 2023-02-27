774ee63529c1b6573f3b757c9508488237d5da9c
Raagaveda <75232496+Raagaveda@users.noreply.github.com>
2023-02-27 14:02:36 +0530

    Add files via upload

---
+++ Makathon/question1.py
@@ -0,0 +1,5 @@
+n,m=map(int,input().split())
+print((n-1)+(m-1))
+
+#As said each should be cut down to 1*1 , for example if the x=4 and y=2 then we can see that there are 3 horizontal lines and 
+# one vertical line so it is clear that subtracting 1 from from both length and width gives us the no of cuts 
